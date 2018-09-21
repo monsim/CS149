1f89355adf4ac40581aa5f33b8f8c854cf94e1a5
StanYanakiev <stan.yanakiev1@gmail.com>
2018-09-20 18:30:55 -0700

    SJF.java done

--- hw2/src/hw2/SJF.java
+++ hw2/src/hw2/SJF.java
@@ -1,160 +1,160 @@
 package hw2;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.*;
 
-///// CS149 - Group 8 /////
+import hw2.Process;
+
 public class SJF {
-	// Fields
-	public String CPUStatus; // a string to represent the status of CPU at every quanta starting from 0 to
-								// 100.
-	public String infoForProcess;
-	public final static int MAX_QUANTA_RUN_TIME = 100;
-	public static int timeCounter = 0; // a counter to keep track of time (from 0 to MAX_QUANTA_RUN_TIME)
-	public Queue<Process> processQueue;
-	public ArrayList<Process> processes;
-	public double responseTime;
+	
+	public String CPUStatus; // status of CPU is recorded at every quanta
+	public final static int MAX_TOTAL_RUN_TIME = 100;
+	public static int timeCount = 0; // tracks time
+	// process
 	public int processDone;
-	public double turnAroundTime;
+	public String processInfo;
+	public ArrayList<Process> processList;
+	public Queue<Process> processQueue;
+	
+	// time
 	public double waitTime;
-
-	// constuctor: sorts processes according to when they are started
+	public double responseTime;
+	public double turnAroundTime;
+	
+	// processList is sorted based on arrivalTime and xpectedRunTime
 	public SJF(ArrayList<Process> processes) {
-		this.infoForProcess = "";
+		this.processInfo = "";
 		this.CPUStatus = "";
-		this.processes = processes;
-		Collections.sort(this.processes,
-				(o1, o2) -> o1.getArrivalTime() < o2.getArrivalTime() ? -1
-						: o1.getArrivalTime() > o2.getArrivalTime() ? 1
-								: o1.getExpectedRunTime() < o2.getExpectedRunTime() ? -1
-										: o1.getExpectedRunTime() > o2.getExpectedRunTime() ? 1 : 0);
-
-		// Ready queue, used to store processes which can be executed by the CPU when
-		// given an
-		// opportunity.
-		ArrayList<Process> sjfProcesses = new ArrayList<>();
-		ArrayList<Process> tempProcesses = (ArrayList<Process>) processes.clone();
-		tempProcesses.remove(0);
+		this.processList = processes;
+		Collections.sort(processList, new Comparator<Process>() {
+			public int compare(Process p1, Process p2) {
+				if (p1.getArrivalTime() < p2.getArrivalTime())
+					return -1;
+				else if(p1.getArrivalTime() > p2.getArrivalTime())
+					return 1;
+				else if(p1.getExpectedRunTime() < p2.getExpectedRunTime())
+					return -1;
+				else if(p1.getExpectedRunTime() > p2.getExpectedRunTime())
+					return 1;
+				else 
+					return 0;		
+			}
+		});
+		
+		// The processes in the queue are ready to be ran by the CPU
+		ArrayList<Process> sjfProcessList = new ArrayList<>();
+		ArrayList<Process> processListCopy = (ArrayList<Process>) processes.clone();
+		processListCopy.remove(0);
 		processQueue = new LinkedList<Process>();
-		processQueue.add(processes.get(0));
+		processQueue.add(processList.get(0));
 
-		// variable time keeps track of total runtime in quanta
-		int time = (int) Math.ceil(processes.get(0).getArrivalTime())
-				+ (int) Math.ceil(processes.get(0).getExpectedRunTime());
+		// the total runtime of quanta
+		int time = (int) Math.ceil(processList.get(0).getArrivalTime())
+				+ (int) Math.ceil(processList.get(0).getExpectedRunTime());
 
-		boolean done = false;
-		while (!done) {
-			// did a new job start before the current job finished?
+		boolean finished = false;
+		while (!finished) {
+			// gets the newly queued up jobs if any
+			// else, the next job is taken from processListCopy
 			boolean jobArrived = false;
 
-			// finds jobs that are queueing up
-			for (int j = 0; j < tempProcesses.size(); j++) {
-				if (tempProcesses.get(j).getArrivalTime() < time) {
-					sjfProcesses.add(tempProcesses.get(j));
+			for (int j = 0; j < processListCopy.size(); j++) {
+				if (processListCopy.get(j).getArrivalTime() < time) {
+					sjfProcessList.add(processListCopy.get(j));
 					jobArrived = true;
 				}
-				// if no jobs in queue, get next one in tempProcesses
 				else if (!jobArrived) {
-					sjfProcesses.add(tempProcesses.get(j));
-					tempProcesses.remove(j);
+					sjfProcessList.add(processListCopy.get(j));
+					processListCopy.remove(j);
 					break;
 				} else
 					break;
 			}
 
-			// uses standard sort() method to find shortest job
-			Collections.sort(sjfProcesses, new Comparator<Process>() {
+			// the next shortest job is found
+			Collections.sort(sjfProcessList, new Comparator<Process>() {
 				@Override
-				public int compare(Process o1, Process o2) {
-					return o1.getExpectedRunTime() < o2.getExpectedRunTime() ? -1
-							: o1.getExpectedRunTime() > o2.getExpectedRunTime() ? 1 : 0;
+				public int compare(Process p1, Process p2) {
+					if(p1.getExpectedRunTime() < p2.getExpectedRunTime())
+						return -1;
+					else if( p1.getExpectedRunTime() > p2.getExpectedRunTime())
+						return 1;
+					else 
+						return 0;
 				}
 			});
 
-			// add shortest job to queue,
-			processQueue.add(sjfProcesses.get(0));
-			// remove that same job from the temp queue
-			tempProcesses.remove(sjfProcesses.get(0));
-			time += (int) Math.ceil(sjfProcesses.get(0).getExpectedRunTime());
-			sjfProcesses = new ArrayList<>();
+			// the shortest job is added to the Queue and removed from the copy
+			processQueue.add(sjfProcessList.get(0));
+			processListCopy.remove(sjfProcessList.get(0));
+			time += (int) Math.ceil(sjfProcessList.get(0).getExpectedRunTime());
+			sjfProcessList = new ArrayList<>();
 
-			// once all jobs are queued, done
-			if (processQueue.size() == processes.size())
-				done = true;
+			// finished when all of the processList is queued
+			if (processQueue.size() == processList.size())
+				finished = true;
 		}
 	}
 
-	// method run() starts the SJF algorithm
+	// runs the implemented SJF
 	public void run() {
-		timeCounter = 0;
-		boolean isRunning = false; // is there a processes currently running? default set to false
-		Process current = null; // temp var
-		double nextAvailability = 0.0; // var to keep track of whether a process is running
-		for (; timeCounter < MAX_QUANTA_RUN_TIME; timeCounter++) {
-			if (nextAvailability <= timeCounter) {
-				// If the timeCounter is greater than or equal to nextAvailability, then the
-				// current process is done
-				isRunning = false;
-				if (current != null) {
-					this.infoForProcess += this.printProcess(current);
-					current = null;
-					this.processDone++;
+		boolean isExecuting = false; // process is running or not
+		Process tempProcess = null;
+		double readyForNext = 0.0; // tracks running time of process
+		for (timeCount = 0; timeCount < MAX_TOTAL_RUN_TIME; timeCount++) {
+			// process is only done if readyForNext exceeds timeCount
+			if (readyForNext <= timeCount) {
+				isExecuting = false;
+				if (tempProcess != null) {
+					processDone++;
+					processInfo += this.toString(tempProcess);
+					tempProcess = null;
 				}
 			}
 
-			// checks if the CPU is idle and if there are any processes in the queue
-			if (!processQueue.isEmpty() && processQueue.peek().getArrivalTime() <= timeCounter && !isRunning) {
-				current = processQueue.poll();
-				isRunning = true;
-				// expectedRunTime = when the current process is expected to complete its run
-				nextAvailability = timeCounter + current.getExpectedRunTime();
-				this.CPUStatus += timeCounter + ": Process " + current.getProcessID() + "\n";
-
-				// time from process start to finish
-				this.turnAroundTime += nextAvailability - current.getArrivalTime();
-				// time in queue
-				this.waitTime += timeCounter - (current.getArrivalTime() + current.getExpectedRunTime());
-				// time from process arrival until start
-				this.responseTime += timeCounter - current.getArrivalTime();
-
+			// calculates turnAroundTime, waitTime, and responseTime based on the job from the processList in the queue
+			if (!processQueue.isEmpty() && processQueue.peek().getArrivalTime() <= timeCount && !isExecuting) {
+				tempProcess = processQueue.poll();
+				isExecuting = true;
+				readyForNext = timeCount + tempProcess.getExpectedRunTime();
+				CPUStatus += timeCount + ": Process " + tempProcess.getProcessID() + "\n";
+				
+				this.turnAroundTime += readyForNext - tempProcess.getArrivalTime();
+				this.waitTime += timeCount - (tempProcess.getArrivalTime() + tempProcess.getExpectedRunTime());
+				this.responseTime += timeCount - tempProcess.getArrivalTime();
+				
+			// finds if CPU is busy or free, waits accordingly 
 			} else {
-				if (current != null) // if CPU is busy,
+				if (tempProcess != null) 
 				{
-					// add process number
-					this.CPUStatus += timeCounter + ": Process " + current.getProcessID() + "\n";
-				} else // if CPU is idle,
+					this.CPUStatus += timeCount + ": Process " + tempProcess.getProcessID() + "\n";
+				} else
 				{
-					// wait for process to run
-					this.CPUStatus += timeCounter + ": Waiting for a process\n";
+					this.CPUStatus += timeCount + ": Waiting for a process\n";
 				}
 
 			}
 
-			// checks whether the process is still running after 100 quanta (time slices)
-			if (isRunning) {
+			// finds if process proceeds the time slice of 100 quantas
+			if (isExecuting) {
 				this.processDone++;
-				for (; timeCounter < Math.round(nextAvailability); timeCounter++) {
-					this.CPUStatus += timeCounter + ": Process " + current.getProcessID() + "\n";
-					this.responseTime += timeCounter;
+				for (; timeCount < Math.round(readyForNext); timeCount++) {
+					this.CPUStatus += timeCount + ": Process " + tempProcess.getProcessID() + "\n";
+					this.responseTime += timeCount;
 				}
-				this.infoForProcess += this.printProcess(current);
+				this.processInfo += this.toString(tempProcess);
 			}
 
-			// print output to txt file about processes
-			System.out.println(this.infoForProcess);
-			System.out.println(this.CPUStatus);
-			System.out.println("\nAverage Turn-Around time: " + this.turnAroundTime / this.processDone);
-			System.out.println("Average Wait time: " + this.waitTime / this.processDone);
-			System.out.println("Average Response time: " + this.responseTime / this.processDone);
-			System.out.println("Throughput: " + this.processDone / (float) timeCounter);
+			// processList info printed to the txt file
+			System.out.println(processInfo);
+			System.out.println(CPUStatus);
+			System.out.println("\nAverage Turn-Around time: " + turnAroundTime / processDone);
+			System.out.println("Average Wait time: " + waitTime / processDone);
+			System.out.println("Average Response time: " + responseTime / processDone);
+			System.out.println("Throughput: " + processDone / (float) timeCount);
 		}
 	}
-
-	// method to return formatted string of a process
-	private String printProcess(Process currProcess) {
+	
+	private String toString(Process currProcess) {
 		return "\nProcess " + currProcess.getProcessID() + " \nArrival Time of current process: "
 				+ currProcess.getArrivalTime() + " \nRun time of current process: " + currProcess.getExpectedRunTime()
 				+ " \nPriority of current process: " + currProcess.getPriority() + "\n";
