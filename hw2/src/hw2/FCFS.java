53a6badd92119c7673ff48364be80596588d2fc1
Pranika Bedi <pranikabedi@Pranikas-MacBook-Pro.local>
2018-09-21 15:12:26 -0700

    pranika commits

--- hw2/src/hw2/FCFS.java
+++ hw2/src/hw2/FCFS.java
@@ -5,121 +5,120 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Collections;
 
-///// CS149 - Group 8 /////
+// CS 149 GROUP 10 \\
 
 public class FCFS
 {
-	// FAAAAAAAAA
-	public String CPUStatus; // a string to represent the status of CPU at every quanta starting from 0 to 100.
-	public String infoForProcess;
+	public String manageCPU; // String represents CPU status at quanta from 0 to 100 
+	public String processInfo; // String that contains the process information
 	public final static int MAX_QUANTA_RUN_TIME = 100;
-	public static int timeCounter = 0; // a counter to keep track of time (from 0 to MAX_QUANTA_RUN_TIME)
-	public Queue<Process> processQueue;
+	public static int timeCount = 0; // Keeps track of time from 0 to 100 
+	public Queue<Process> processChain; //processQueue
 	public ArrayList<Process> processes;
 	public double responseTime;
-	public int processDone;
+	public int processEnd;//processDone
 	public double turnAroundTime;
 	public double waitTime;
 
-
-	// constructor that sorts processes according to when they are started, using Collections.sort()
-	public FCFS( ArrayList<Process> processes )
+	// Using Collections.sort() processes are sorted in ascending order depending on when they started 
+	public FCFS(ArrayList<Process> processes)
 	{
-		this.infoForProcess = "";
-		this.CPUStatus = "";
 		this.processes = processes;
-		Collections.sort(this.processes, (o1, o2) -> {
-            // compare processes against one another based on arrival time
-            return o1.getArrivalTime() < o2.getArrivalTime() ? -1 :
-                 o1.getArrivalTime() > o2.getArrivalTime() ? 1: 0;
+		this.manageCPU = "";
+		this.processInfo = "";
+		Collections.sort(this.processes, (o1, o2) -> {              
+			if (o1.getArrivalTime() < o2.getArrivalTime()){
+				return -1;
+			}
+			else if (o1.getArrivalTime() > o2.getArrivalTime()){
+				return 1;
+			}
+			else
+			{
+				return 0;
+			}
         });
 
-		// Ready queue, used to store processes which can be executed by the CPU when given an
-		// opportunity.
-		processQueue = new LinkedList<Process>();
-		for(Process proc: this.processes){
-			processQueue.add(proc);
+		// This queue of processes will be executed by the CPU
+		processChain = new LinkedList<Process>();
+		for(Process processOne: this.processes){
+			processChain.add(processOne);
 		}
 	}
 
 	// Method run() starts the FCFS algorithm
 	public void run()
 	{
-		// counter (using quanta as scale)
-		timeCounter = 0;
-		boolean isRunning = false; // is there a processes currently running? default set to false
-		Process current = null; // temp var
-		double nextAvailability = 0.0; // var to keep track of whether a process is running
-		for (; timeCounter < MAX_QUANTA_RUN_TIME; timeCounter++)
+		timeCount = 0;
+		boolean isRunning = false; // Boolean var to represent if process is still running
+		Process currentProcess = null; 
+		double nextAvailable = 0.0; // Keeps track of running processes
+		for (; timeCount < MAX_QUANTA_RUN_TIME; timeCount++)
 		{
-			if (nextAvailability <= timeCounter)
+			if (nextAvailable <= timeCount) // if true, then current process is finished
 			{
-				// If the timeCounter is greater than or equal to nextAvailability, then the current process is done
 				isRunning = false;
-				if (current != null) {
-					this.infoForProcess += this.printProcess(current);
-					current = null;
-					this.processDone++;
+				if (currentProcess != null) {
+					this.processInfo = this.processInfo + this.printProcess(currentProcess);
+					currentProcess = null;
+					this.processEnd++;
 				}
 			}
-			// checks if the CPU is idle and if there are any processes in the queue
-			if (!processQueue.isEmpty() && processQueue.peek().getArrivalTime() <= timeCounter && !isRunning)
+			// Checks if there are any processes in the queue and whether CPU is running or idle
+			if (!processChain.isEmpty() && processChain.peek().getArrivalTime() <= timeCount && !isRunning)
 			{
-				current = processQueue.poll();
+				currentProcess = processChain.poll();
 				isRunning = true;
-				// expectedRunTime = when the current process is expected to complete its run
-				nextAvailability = timeCounter + current.getExpectedRunTime();
-				this.CPUStatus += timeCounter + ": Process " + current.getProcessID() +"\n";
-
-				// time from process start to finish
-				this.turnAroundTime += nextAvailability - current.getArrivalTime();
-				// time in queue
-				this.waitTime += timeCounter - (current.getArrivalTime() + current.getExpectedRunTime());
+				nextAvailable = timeCount + currentProcess.getExpectedRunTime(); // Expected run time is when the current process is expected to end 
+				this.manageCPU = this.manageCPU + timeCount + ": Process " + currentProcess.getProcessID() +"\n";
+				// Process start time to finish time 
+				this.turnAroundTime += nextAvailable - currentProcess.getArrivalTime();
+				// Time in queue
+				this.waitTime += timeCount - (currentProcess.getArrivalTime() + currentProcess.getExpectedRunTime());
 				// time from process arrival until start
-				this.responseTime += timeCounter - current.getArrivalTime();
+				this.responseTime += timeCount - currentProcess.getArrivalTime();
 			}
 			else
 			{
-				if (current != null) // if CPU is busy,
+				if (currentProcess != null) // Checks if the CPU is busy 
 				{
-					// add process number
-					this.CPUStatus += timeCounter + ": Process " + current.getProcessID() +  "\n";
+					// Adds process ID number
+					this.manageCPU = this.manageCPU + timeCount + ": Process " + currentProcess.getProcessID() +  "\n";
 				}
-				else // if CPU is idle,
+				else // CPU is idle
 				{
-					// wait for process to run
-					this.CPUStatus += timeCounter + ": Waiting for a process\n";
+					// Waits for a process
+					this.manageCPU = this.manageCPU + timeCount + ": Waiting for a process to run\n";
 				}
 				
 			}
 		}
-		// checks whether the process is still running after 100 quanta (time slices)
+		// Checks if process is still running after 100 quanta
 		if (isRunning)
 		{
-			this.processDone++;
-			for (; timeCounter < Math.round(nextAvailability); timeCounter++) {
-				this.CPUStatus += timeCounter + ": Process " + current.getProcessID() +  "\n";
-				this.responseTime += timeCounter;
+			this.processEnd++;
+			for (; timeCount < Math.round(nextAvailable); timeCount++) {
+				this.manageCPU = this.manageCPU + timeCount + ": Process " + currentProcess.getProcessID() +  "\n";
+				this.responseTime = this.responseTime + timeCount;
 			}
-			this.infoForProcess += this.printProcess(current);
+			this.processInfo = this.processInfo + this.printProcess(currentProcess);
 		}
 		
-		// print output to txt file about processes
-		System.out.println(this.infoForProcess);
-		System.out.println(this.CPUStatus);
-		System.out.println("\nAverage Turn-Around time: " + this.turnAroundTime/this.processDone);
-		System.out.println("Average Wait time: " + this.waitTime/this.processDone);
-		System.out.println("Average Response time: " + this.responseTime/this.processDone);
-		System.out.println("Throughput: " + this.processDone/ (float) timeCounter);
+		// Output printed on txt files
+		System.out.println(this.processInfo);
+		System.out.println(this.manageCPU);
+		System.out.println("\nAverage Turn-Around Time is: " + this.turnAroundTime/this.processEnd);
+		System.out.println("Average Wait Time is: " + this.waitTime/this.processEnd);
+		System.out.println("Average Response Time is: " + this.responseTime/this.processEnd);
+		System.out.println("Throughput: " + this.processEnd/ (float) timeCount);
 	}
 
-	////
 
-	// method to return formatted string of a process
+	// Returns formatted strings of the processes
 	private String printProcess(Process currProcess) {
 			return "\nProcess " + currProcess.getProcessID() +
-					" \nArrival Time of current process: " + currProcess.getArrivalTime() +
-					" \nRun time of current process: " + currProcess.getExpectedRunTime() +
-					" \nPriority of current process: " + currProcess.getPriority() + "\n";
+					" \nArrival Time of current process is: " + currProcess.getArrivalTime() +
+					" \nRun Time of current process is: " + currProcess.getExpectedRunTime() +
+					" \nPriority of current process is: " + currProcess.getPriority() + "\n";
 	}
 }
