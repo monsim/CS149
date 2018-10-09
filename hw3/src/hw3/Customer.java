d137e291862aaed657ddd4f0342aee5770be16a1
monsim <monsi5pen@gmail.com>
2018-10-08 23:15:22 -0700

    customer and sellerh done

--- hw3/src/hw3/Customer.java
+++ hw3/src/hw3/Customer.java
@@ -2,119 +2,131 @@ package hw3;
 
 import java.util.Random;
 
-public class Customer implements Comparable<Customer>
-{
-    Random random = new Random();
-    private int arrivalTime;
-    private int seatNum;
-    private int customerID;
+public class Customer implements Comparable<Customer> {
 
+	private int currentTime;
+	private int arrivalTime;
+	Random randomGen = new Random();
 	private String ticket;
-    private int time;
+	private int seatNumber;
+	private int customerID;
 
-    /**
-     * Getter method to access time
-     * @return int time
-     */
-    public int getTime() {
-        return time;
-    }
+	/**
+	 * Sets customer id and generates random arrival time for testing of this
+	 * program
+	 * 
+	 * @param int
+	 *            customerID
+	 */
+	public Customer(int customerID) {
+		arrivalTime = randomGen.nextInt(60); // arrives between 0 and 60 minutes
+		this.customerID = customerID;
+		seatNumber = -1; // not seated originally
+	}
 
-    /**
-     * Setter method for time
-     * @param int time to set this.time
-     */
-    public void setTime(int time) {
-        this.time = time;
-    }
+	/**
+	 * returns arrival time
+	 * 
+	 * @return arrival time
+	 */
+	public int getArrivalTime() {
+		return this.arrivalTime;
+	}
 
-    /**
-     * Getter method to access ticket
-     * @return String ticket
-     */
-    public String getTicket() {
-        return ticket;
-    }
-
-    /**
-     * Setter method for time
-     * @param String ticket to set this.ticket
-     */
-    public void setTicket(String ticket) {
-        this.ticket = ticket;
-    }
-
-    /**
-     * Sets customer id and generates random arrival time for testing of this program
-     * @param int customerID
-     */
-    public Customer(int customerID){
-        arrivalTime = random.nextInt(60);
-        this.customerID = customerID;
-        seatNum = -1;
-    }
-
-    /**
-     * Getter method to arrival time
-     * @return int arrivalTime
-     */
-    public int getArrivalTime(){
-        return this.arrivalTime;
-    }
-    
-    /**
-     * Setter method for seat number
-     * @param int seatNum to set this.seatNum
-     */
-    public void setSeatNum(int seatNum){
-        this.seatNum = seatNum;
-    }
-
-    /**
-     * Checks to see if a seat number is has been assigned to a customer
-     * @return false if seat number is not assigned, else true
-     */
-    public boolean isSigned()
-    {
-        if(seatNum== -1)  return false;
-        else return true;
-    }
-    
-    /**
-     * Getter method to access seat number
-     * @return int seatNum
-     */
-    public int getSeatNum(){
-        return this.seatNum;
-    }
-
-    /**
-     * Getter method to access customer's id number
-     * @return int customerID
-     */
-    public int customerID(){
-        return this.customerID;
-    }
-    
-    public int getCustomerID() {
+	/**
+	 * returns the Customer's id
+	 * 
+	 * @return customer id
+	 */
+	public int getCustomerID() {
 		return customerID;
 	}
 
+	/**
+	 * sets the Customer's id
+	 * 
+	 * @param customerID customer id
+	 */
 	public void setCustomerID(int customerID) {
 		this.customerID = customerID;
 	}
 
-    /**
-     * Compares arrival time of 2 customers
-     */
-    @Override
-    public int compareTo(Customer customer)
-    {
-        if(this.arrivalTime < customer.arrivalTime)
-            return -1;
-        else if(this.arrivalTime > customer.arrivalTime)
-            return 1;
-        else
-            return 0;
-    }
+	/**
+	 * returns the time
+	 * 
+	 * @return time
+	 */
+	public int getTime() {
+		return currentTime;
+	}
+
+	/**
+	 * sets the time
+	 * 
+	 * @param time to set to
+	 */
+	public void setTime(int time) {
+		this.currentTime = time;
+	}
+
+	/**
+	 * gets ticket info
+	 * 
+	 * @return ticker info
+	 */
+	public String getTicket() {
+		return ticket;
+	}
+
+	/**
+	 * assigns ticket to Customer
+	 * 
+	 * @param ticket
+	 */
+	public void setTicket(String ticket) {
+		this.ticket = ticket;
+	}
+
+	/**
+	 * assigns seat to Customer
+	 * 
+	 * @param seat number to assign
+	 */
+	public void setSeatNumber(int seatNum) {
+		this.seatNumber = seatNum;
+	}
+
+	/**
+	 * returns the seat number assigned to this customer
+	 * 
+	 * @return int seatNum
+	 */
+	public int getSeatNumber() {
+		return this.seatNumber;
+	}
+
+	/**
+	 * returns whether the Customer is seated or not
+	 * 
+	 * @return true if they're seated, false if not
+	 */
+	public boolean isSeated() {
+		if (seatNumber == -1)
+			return false;
+		else
+			return true;
+	}
+
+	/**
+	 * compares Customers based on their arrival times (used for queue)
+	 */
+	@Override
+	public int compareTo(Customer customer) {
+		if (this.arrivalTime < customer.arrivalTime)
+			return -1;
+		else if (this.arrivalTime > customer.arrivalTime)
+			return 1;
+		else
+			return 0;
+	}
 }
--- hw3/src/hw3/SellerH.java
+++ hw3/src/hw3/SellerH.java
@@ -3,70 +3,50 @@ package hw3;
 public class SellerH extends Seller
 {
     private Object lock;
-    public SellerH(Seat[][] seat, String sellerID, Object lock)
-    {
-        // Seller H takes 1 or 2 minutes to complete a ticket sale
-        super(seat, randomVariable.nextInt(2) + 1, sellerID, lock, System.currentTimeMillis());
+    
+    public SellerH(Seat[][] seat, String sellerID, Object lock) {
+        super(seat, randomVariable.nextInt(2) + 1, sellerID, lock, System.currentTimeMillis());	//1 or 2 minutes to sell ticket
         this.lock = lock;
     }
 
+    //method to sell a ticket to a customer and assign the customer to a seat
     public void sell() {
         while (!customerQueue.isEmpty()) {
-            //Object lock = new Object();
-
             Customer customer = null;
-            if (customerQueue.isEmpty())
-                return;
-            // Get customer in queue that is ready
+            if (customerQueue.isEmpty()) return;
             update();
-
-            if(currentTime <= 59)
+            if(currentTime < 60)
                 customer = customerQueue.peek();
             else
                 return;
-            // Find seat for the customer
-            // Case for Seller H
             Seat seat = null;
-
-            //System.out.println(currentTime);
-
-            synchronized(lock)
-            {
-
+            synchronized(lock) {
                 update();
-                if(currentTime  >= (customer.getArrivalTime()))
-                {
-                    find_seat:
+                if(currentTime  >= (customer.getArrivalTime())) {
+                    assignSeat:
                     for (int i = 0; i < seating.length; i++) {
                         for (int j = 0; j < seating[0].length; j++) {
+                        		//if the seat is empty, give it to a Customer
                             if (seating[i][j].isSeatEmpty()) {
-                                // Assign seat to customer
-                                // Seat number = (Row x 10) + (Col + 1)
                                 int seatNum = (i*10)+j+1;
                                 seat = new Seat(seatNum);
                                 super.assignSeat(customer, seat, i, j);
-                                //update();
                                 printMsg(customer, seat);
                                 customerQueue.remove();
-
-                                break find_seat;
+                                break assignSeat;		//don't need to continue to find seats
                             }
                         }
                     }
                 }
             }
-            if(seat != null){
+            if(seat != null) {
                 try {
                     Thread.sleep(serviceTime * 1000);
                     update();
-                } catch (InterruptedException e)
-                {
+                } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
-
-
         }
     }
-
 }
